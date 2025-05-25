ceaac33d1949570479075308d1f30e3f02b2f97f
Hoerin Doh <hellodhlyn@gmail.com>
2025-05-25 16:37:05 +0900

    chore: configure for Cloudflare Pages deployment

--- app/root.tsx
+++ app/root.tsx
@@ -11,16 +11,10 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -29,6 +23,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#171717" />
+        <meta name="background-color" content="#171717" />
         <Meta />
         <Links />
       </head>
@@ -42,7 +38,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <div className="bg-neutral-900 min-h-dvh w-screen">
+      <Outlet />
+    </div>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "14\" Palette | Digital Creatives" },
+    { name: "description", content: "Digital creatives, making the world vibrant" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex flex-col items-center justify-center h-dvh">
+      <h1 className="bg-gradient-to-r from-cyan-400 via-blue-400 to-blue-600 text-transparent bg-clip-text text-6xl font-bold">14" Palette</h1>
+      <p className="my-4 text-neutral-100 text-lg">
+        Digital creatives, making the world <span className="bg-gradient-to-br from-purple-500 to-pink-500 text-transparent bg-clip-text font-medium">vibrant</span>
+      </p>
+    </div>
+  );
 }
