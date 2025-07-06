83506a708701bb9b8bc06ae911fabd941d273a08
Hoerin Doh <hellodhlyn@gmail.com>
2025-07-06 23:13:57 +0900

    chore: set biome lint

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Index from "./routes/_index";
 
+// biome-ignore lint/style/noNonNullAssertion: `root` element is always present
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
